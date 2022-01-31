402400e644d95257b9f66a3021b42a7853901678
Matthew <matthewrr@users.noreply.github.com>
2022-01-31 11:56:44 -0800

    Populate weather data based on user ZIP Code input

--- temperature/views.py
+++ temperature/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
-import json, requests
+import requests
 
-def get_grid(lat, long):
-    url = f'https://api.weather.gov/points/{lat},{long}'
+def get_coordinates(zip_code):
+    key = 'fiyonYjyfbmVxjmLOGaeLdmscFXiBGYa'
+    url = f'http://www.mapquestapi.com/geocoding/v1/address?key={key}&location={zip_code}' 
+    response = requests.get(url).json()
+    coordinates = response['results'][0]['locations'][0]['latLng']
+    return coordinates
+
+def get_grid(lat, lng):
+    url = f'https://api.weather.gov/points/{lat},{lng}'
     response = requests.get(url).json()
     return response['properties']['forecast']
 
-def get_forecast(request):
-    lat, long = 48, -117
+def get_forecast(request, zip_code=10001):
+    #add validation for zip code
     if request.method == 'POST':
         zip_code = request.POST['zip_code']
-    grid_url = get_grid(lat, long)
+    coordinates = get_coordinates(zip_code)
+    grid_url = get_grid(coordinates['lat'], coordinates['lng'])
     response = requests.get(grid_url).json()
     return render(request, 'temperature/temperature.html', {'response': response})
